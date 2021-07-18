1944c81cbef1d0e9ff6e04e7bef3a9b3bc94c00a
tivoyudha <tivo.yudhap11@gmail.com>
2021-07-18 23:27:43 +0700

    add binding in example

--- app/src/main/java/com/natassm/calendarpicker/MainActivity.kt
+++ app/src/main/java/com/natassm/calendarpicker/MainActivity.kt
@@ -2,10 +2,15 @@ package com.natassm.calendarpicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.natassm.calendarpicker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(LayoutInflater.from(this)) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
