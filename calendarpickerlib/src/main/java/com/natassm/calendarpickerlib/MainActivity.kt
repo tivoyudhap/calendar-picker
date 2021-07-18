7705766a550c9e7c6f34cba7394a5d8caf907327
tivoyudha <tivo.yudhap11@gmail.com>
2021-07-18 21:54:24 +0700

    init

---
+++ calendarpickerlib/src/main/java/com/natassm/calendarpickerlib/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.natassm.calendarpickerlib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
